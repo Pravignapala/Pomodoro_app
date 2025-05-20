9f36d9d3fb657cc0bf9cccc0b89aaea13012bf6f
Pravignapala <61825172+Pravignapala@users.noreply.github.com>
2025-05-20 17:25:13 -0500

    fix the problem 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/pomodoro-app/',
-  publicDir: 'public'
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
