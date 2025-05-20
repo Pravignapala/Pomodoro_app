e9c28af4bbb3cc468fe84f2af3ebda26e10493aa
Pravignapala <61825172+Pravignapala@users.noreply.github.com>
2025-05-20 22:31:59 +0000

    Deploying to gh-pages from @ Pravignapala/Pomodoro_app@c9b6c020a279dfedfe771a5a818c2e7b9f1af34f 🚀

--- sw.js
+++ sw.js
@@ -23,6 +23,9 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', event => {
+  // Only handle http(s) requests
+  if (!event.request.url.startsWith('http')) return;
+  
   event.respondWith(
     caches.match(event.request)
       .then(response => {
