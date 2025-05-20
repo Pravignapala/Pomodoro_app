1828e3e874b82f37cacae01283598a2a57886ccb
Pravignapala <61825172+Pravignapala@users.noreply.github.com>
2025-05-20 17:21:47 -0500

    fix the problem

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,18 @@
 const CACHE_NAME = 'pomodoro-app-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/sounds/bell.mp3',
-  '/sounds/chime.mp3',
-  '/icons/icon-72x72.png',
-  '/icons/icon-96x96.png',
-  '/icons/icon-128x128.png',
-  '/icons/icon-144x144.png',
-  '/icons/icon-152x152.png',
-  '/icons/icon-192x192.png',
-  '/icons/icon-384x384.png',
-  '/icons/icon-512x512.png'
+  '/pomodoro-app/',
+  '/pomodoro-app/index.html',
+  '/pomodoro-app/manifest.json',
+  '/pomodoro-app/sounds/bell.mp3',
+  '/pomodoro-app/sounds/chime.mp3',
+  '/pomodoro-app/icons/icon-72x72.png',
+  '/pomodoro-app/icons/icon-96x96.png',
+  '/pomodoro-app/icons/icon-128x128.png',
+  '/pomodoro-app/icons/icon-144x144.png',
+  '/pomodoro-app/icons/icon-152x152.png',
+  '/pomodoro-app/icons/icon-192x192.png',
+  '/pomodoro-app/icons/icon-384x384.png',
+  '/pomodoro-app/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
@@ -25,7 +25,22 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(response => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+          return response;
+        });
+      })
   );
 });
 
